b0c6d331a570a073cb747985c54d9f0fe5dea93d
I-Joaquin-I <jfrt853483@gmail.com>
2026-03-06 20:31:49 -0500

    crear componente UserCards y renderizar lista de usuarios en APP

---
+++ frontend/src/components/UserCards.tsx
@@ -0,0 +1,17 @@
+import '../stylesheets/UserCards.css';
+
+interface Props {
+  name: string;
+  age: number;
+}
+
+const UserCards = ({name, age}:Props) => {
+  return (
+    <div className="UserCards">
+      <h1 className="UserCards-tittle">{name}</h1>
+      <h2 className="UserCards-subtittle">{age}</h2>
+    </div>
+  );
+};
+
+export default UserCards;
