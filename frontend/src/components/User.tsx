79b7e33eb88774dec3e81415af0a66dbd2a61d2a
II-Joaquin-II <U22221498@UTP.EDU.PE>
2026-03-08 17:01:54 -0500

    configuracion de rutas dinámicas con ID en los botones editar y eliminar

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -17,10 +17,10 @@ const User = ({ user }: UserProps) => {
                 <h2 className="text font-semibold">{user.name}</h2>
                 <div className="text-sm">Edad: {user.age}</div>
                 <div className="mt-2 flex gap-4">
-                    <Link to={"/edit"} className="inline-block w-full text-center shadow-md text-sm bg-gray-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-gray-600 hover:cursor-pointer">
+                    <Link to={`/edit/${user.id}`} className="inline-block w-full text-center shadow-md text-sm bg-gray-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-gray-600 hover:cursor-pointer">
                         Editar
                     </Link>
-                    <Link to={"/edit"} className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-red-600 hover:cursor-pointer">
+                    <Link to={`/delete/${user.id}`} className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-red-600 hover:cursor-pointer">
                         Eliminar
                     </Link>
                 </div>
