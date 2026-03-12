fe4173a2dd3e25819a8fbf2eaf5cb00e76fba420
II-Joaquin-II <U22221498@UTP.EDU.PE>
2026-03-11 20:08:22 -0500

    Reestructuración de la carpeta pages

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import User from "../User";
+import User from "../components/User";
 import { Link } from "react-router-dom";
 
 interface User {
