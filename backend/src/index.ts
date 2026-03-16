4d78def1348876d8707ea278e85c76b489aa306b
II-Joaquin-II <U22221498@UTP.EDU.PE>
2026-03-15 19:55:17 -0500

    Implementacion de base de datos usando prisma version 6

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,18 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors'; 
 import * as UserController from './controllers/user'; 
 
 const app = express();
+// middlewares
+app.use(cors()); 
 app.use(express.json());
 
 // ruta de prueba
-app.get('/api', (req, res) => res.json('Hola'));
-
-
-//listado de usuarios
-app.get('/api/users', UserController.getUsers);
+app.get('/api', (req, res) => res.json('Hola API conectada'));
 
 //operaciones de usuarios CRUD 
+app.get('/api/users', UserController.getUsers);
 app.get('/api/users/:id', UserController.getUserById);
 app.post('/api/users', UserController.createUser);
 app.put('/api/users/:id', UserController.updateUser);
